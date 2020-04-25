c17ea9328f6d92bee1df74e810e515d3b534c14a
Kat <kmneil2@gmail.com>
2020-04-25 17:52:09 -0400

    install reg redux, not recognizing module

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-# from .models import 
+from .models import Patient, Meds
 
-# admin.site.register(Project)
+admin.site.register(Patient)
+admin.site.register(Meds)
