a2a3f4ffdcbba0a036bd8b06c738dbb8d051a52d
Kat <kmneil2@gmail.com>
2020-05-06 14:08:13 -0400

    registration redux working

--- congif/urls.py
+++ congif/urls.py
@@ -23,31 +23,17 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('registration.backends.default.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
     path('', include('django_registration.backends.activation.urls')),
-    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view, name='password_change_done'),
-    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    # path('accounts/', include('registration.backends.simple.urls')),
-    # path('accounts/login', views.login_view, name='login'),
-    # path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html')),
-    # path('', include('django_registration.backends.activation.urls')),
+    # path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view, name='password_change_done'),
+    # path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', views.dashboard, name='dashboard'),
     path('add-med/', views.add_med, name="add_med"),
-    # path('change-password/', auth_views.PasswordChangeView.as_view()),
-    # path('accounts/password_change/done/',
-    #      auth_views.PasswordChangeDoneView.as_view, name='password_change_done'),
-    # path('accounts/password_reset/',
-    #      auth_views.PasswordResetView.as_view(), name='password_reset'),
-    # path('accounts/password_reset/done/',
-    #      auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    # path('accounts/reset/<uidb64>/<token>/',
-    #      auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    # path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(),
-    #      name='password_reset_complete'),
-
+    
 ]
 
 
