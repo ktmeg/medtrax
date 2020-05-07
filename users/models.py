028660de0739e728b48d374269a118471b25a0dd
Brandon <bmiller3822@gmail.com>
2020-05-07 15:50:13 -0400

    Progress on registration styling.

--- users/models.py
+++ users/models.py
@@ -11,6 +11,7 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=20, blank=False)
     last_name = models.CharField(max_length=20, blank=False)
     email = models.EmailField(max_length=254, unique=True, blank=True)
+    silliness = models.CharField(max_length=20, blank=False)
 
     USERNAME_FIELD = 'username'
 
