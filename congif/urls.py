72e2f66b76d6b696807d42726662004f29674970
Brandon <bmiller3822@gmail.com>
2020-05-28 12:12:45 -0400

    import abstractuser

--- congif/urls.py
+++ congif/urls.py
@@ -1,18 +1,3 @@
-"""congif URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
@@ -26,7 +11,8 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     # path('accounts/', include('registration.backends.default.urls')),
     # path('', include('django_registration.backends.activation.urls')),
-    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_change/',
+         auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('accounts/password_change/done/',
          auth_views.PasswordChangeDoneView.as_view, name='password_change_done'),
     path('accounts/password_reset/',
