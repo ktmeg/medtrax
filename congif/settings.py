7ff468b933ae047d026012a29efe8ac63a08a058
Brandon <bmiller3822@gmail.com>
2020-05-28 11:54:20 -0400

    trying the FloorPlan way

--- congif/settings.py
+++ congif/settings.py
@@ -60,9 +60,7 @@ ROOT_URLCONF = 'congif.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-        ],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
