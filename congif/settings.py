c77c949728bfd9cdc7f8911c6324e0f615f5b2d1
Brandon <bmiller3822@gmail.com>
2020-05-29 14:27:35 -0400

    Trying what documentation said

--- congif/settings.py
+++ congif/settings.py
@@ -125,9 +125,12 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = "/static/"
 
 # Custom user model
 
